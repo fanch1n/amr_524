355653511b9a96671947bcaac6f632a920e63cdd
Fan Chen <fanchencf@gmail.com>
2022-10-18 17:50:14 -0400

    initial commit solver

--- field.py
+++ field.py
@@ -1,4 +1,4 @@
 import numpy as np
 
-if __name__ == “main”:
+if __name__ == “__main__”:
 
---
+++ solver.py
@@ -0,0 +1,29 @@
+import numpy as np
+# input: user defined PDE information (type, parameter)
+# method: gen_solver, finite_difference()
+def get_solver_options(solver_options):
+     # default_solver_options = {'solver':diffusion, 'D':1.e-3, 'max_iters':1000000}
+     # default_solver_options.update(solver_options)
+     # return default_solver_options
+     return 
+
+def diffusion():
+    
+    return 
+
+def spinodal_decomposition():
+
+    return 
+
+def gen_solver(problem, bondx):
+    def solver():
+        return
+    return 
+
+def finite_difference():
+
+    return 
+
+def evolve():
+
+    return 
