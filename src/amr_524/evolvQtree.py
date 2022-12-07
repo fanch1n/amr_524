d08259349304ac964e4b02a4256884eeb0aa6014
Arrondissement5etDemi <hainaw@princeton.edu>
2022-12-07 01:50:27 -0500

    lots of stuff

--- src/amr_524/evolvQtree.py
+++ src/amr_524/evolvQtree.py
@@ -1,79 +1,43 @@
 from mesh import QTree
-import copy 
 import numpy as np
-import matplotlib.pyplot as plt
-import pprint
 
-def evolve(qtree, bondx='OPEN'): #evolve one step
+def evolve_diffusion(qtree): #evolve one step
     nodes = qtree.root.find_children()
-    mesh_new = np.copy(qtree.domain) #new field
-    Lx, Ly = qtree.domain.shape[0], qtree.domain.shape[1]
-    laplacian = lambda prev, curr, nxt: nxt - 2 * curr + prev
-    dx, dy, dt = 1, 1, 0.01 #FIXME 
-    for node in nodes:
-        # calculate gradient only for points in qtree
-        x, y = node.x0, node.y0
-        val = 0
-        if bondx == 'OPEN':
-            if x > 0 and x < Lx - 1 and y > 0 and y < Ly - 1:
-                lap_x = laplacian(qtree.domain[x-1, y], qtree.domain[x, y], qtree.domain[x+1, y])
-                lap_y = laplacian(qtree.domain[x, y-1], qtree.domain[x, y], qtree.domain[x, y+1])
-                val = dt * (lap_x/dx + lap_y/dy) 
-        elif bondx == 'PBC':  
-            lap_x = laplacian(qtree.domain[(x-1)%Lx, y],\
-                              qtree.domain[x, y], qtree.domain[(x+1)%Lx, y])
-            lap_y = laplacian(qtree.domain[x, (y-1)%Ly],\
-                              qtree.domain[x, y], qtree.domain[x, (y+1)%Ly])
-            val = dt * (lap_x/dx + lap_y/dy) 
-        else:
-            raise Exception("missing specification of boundary condition")
-        c = node.get_points(mesh_new)
-        for item in c:
-            item += val
-    return mesh_new
-
-def evolve_diffusion(qtree, bondx = 'OPEN'): #evolve one step
-    nodes = qtree.root.find_children()
-    old_field = qtree.domain #new field
+    old_field = qtree.domain #old field
+    bondx = qtree.bondx
+    CELL_MIN = qtree.minCellSize
     mesh_new = np.copy(old_field) #new field
     Lx, Ly = old_field.shape[0], old_field.shape[1]
-    laplacian = lambda prev, curr, nxt: nxt - 2 * curr + prev
+    laplacian = lambda prev, curr, nxt, cellmin: (nxt - 2 * curr + prev)/cellmin
     dx, dy, dt = 1, 1, 0.01 #FIXME 
     node_values = dict()
     for node in nodes:
         # calculate gradient only for points in qtree
         corners = node.get_corners()
-        avr = 0
         for corner in corners:
             x, y = corner[0], corner[1]
             if (x, y) not in node_values:
                 lap_x, lap_y = 0, 0
                 if bondx == 'OPEN':
-                    if x > 0 and x < Lx - 1 and y > 0 and y < Ly - 1:
-                        lap_x = laplacian(old_field[x - 1, y], old_field[x, y], old_field[x + 1, y])
-                        lap_y = laplacian(old_field[x, y - 1], old_field[x, y], old_field[x, y + 1])
+                    if x > CELL_MIN - 1 and x < Lx - CELL_MIN and y > CELL_MIN - 1 and y < Ly - CELL_MIN:
+                        lap_x = laplacian(old_field[x - CELL_MIN, y], old_field[x, y], old_field[x + CELL_MIN, y], CELL_MIN)
+                        lap_y = laplacian(old_field[x, y - CELL_MIN], old_field[x, y], old_field[x, y + CELL_MIN], CELL_MIN)
                 elif bondx == 'PBC':  
-                    lap_x = laplacian(old_field[(x-1)%Lx, y],\
-                                    old_field[x, y], old_field[(x+1)%Lx, y])
-                    lap_y = laplacian(old_field[x, (y-1)%Ly],\
-                                    old_field[x, y], old_field[x, (y+1)%Ly]) 
+                    lap_x = laplacian(old_field[(x - CELL_MIN)%Lx, y],\
+                                    old_field[x, y], old_field[(x + CELL_MIN)%Lx, y], CELL_MIN)
+                    lap_y = laplacian(old_field[x, (y - CELL_MIN)%Ly],\
+                                    old_field[x, y], old_field[x, (y + CELL_MIN)%Ly], CELL_MIN) 
                 else:
-                    raise Exception("missing specification of boundary condition")
+                    raise Exception("missing specification of boundary condition")   
                 mesh_new[x, y] += dt * (lap_x/dx + lap_y/dy) 
                 node_values[(x, y)] = mesh_new[x, y]
-            with np.nditer(node.get_corners_points(mesh_new, corners.index(corner)), 
-            op_flags=['readwrite']) as it:
-                for item in it:
-                    item[...] = node_values[(x, y)]
-            #avr += node_values[(x, y)]
-        #avr = avr/4.0  
-        #node.get_points(mesh_new).fill(avr)
+            node.get_corners_points(mesh_new, corners.index(corner)).fill(node_values[(x, y)])
     return mesh_new
 
 if __name__ == '__main__':
     # Define/load constant parameters
-    EPS = 1e-3
-    CELL_MIN = 3
+    EPS = 1e-6
+    CELL_MIN = 4
     bondx = "PBC"
     # Load a sample 2D matrix
     domain = np.load("lib/circle.npy")
@@ -85,11 +49,10 @@ if __name__ == '__main__':
     nodes = test_domain.root.find_children()
     new_mesh = test_domain.domain 
    
-    for step in range(1, 5000):
-        #new_mesh = evolve(test_domain)
-        new_mesh = evolve_diffusion(test_domain, bondx)
-        if step < 10 or step % 10 == 0:  
-            print(step, np.max(new_mesh.flatten()))
+    for step in range(1, 10000):
+        new_mesh = evolve_diffusion(test_domain)
+        if step < 20 or step % 500 == 0:  
+            print(step, np.mean(new_mesh.flatten()))
             test_domain = QTree(EPS, CELL_MIN, new_mesh, bondx)
             test_domain.subdivide()
             if step < 20 or step % 500 == 0:
